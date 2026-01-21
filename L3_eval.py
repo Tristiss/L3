7cba6a4884122fa339e81527eea1d78084a0080a
Tristis <henhov112@gmail.com>
2026-01-21 21:43:26 +0100

    eval design improved and more features added, programm uses all files in directory automatically

--- L3_eval.py
+++ L3_eval.py
@@ -6,6 +6,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.interpolate import make_smoothing_spline
+from os import listdir
+from os.path import isfile, join
 
 # enable latex in plots
 mpl.rcParams['text.usetex'] = True
@@ -16,102 +18,111 @@ locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
 num = 11
 test_phase = True
 
+path = r"C:\Programmieren\Praktikum\L3\Data"
+
 u_current = 10
 
 def u_voltage(volt):
     return volt * 0.001 + 0.05
 
-def single_evaluation(num, axs, rev_bias, ax_3d, og_kind = True):
-    if og_kind == True:
-        if num == 5:
-            df = pd.read_csv(rf"C:\Programmieren\Praktikum\L3\Data\Messung_{num}.csv", names = ["voltages", "currents"], sep = ";")    
-            voltages = list(df["voltages"][1:])
-            currents = list(df["currents"][1:])
-        else:
-            df = pd.read_csv(rf"C:\Programmieren\Praktikum\L3\Data\Messung_{num}.csv", sep = ";")
-            voltages = list(df["voltages"])
-            currents = list(df["currents"])
-    else:
-        df = pd.read_csv(rf"C:\Programmieren\Praktikum\L3\Data\Messung_rev_bias_{rev_bias}_{num}.csv", sep = ";")
-        voltages = list(df["voltages"])
-        currents = list(df["currents"])
-    
+def csv_read(filename):
+    df = pd.read_csv(path + r"\\" + filename, sep = ";")
+    voltages = list(df["voltages"])
+    currents = list(df["currents"])
+    return voltages, currents
+
+def single_evaluation(voltages, currents, axs, rev_bias, ax_3d):
     spl = make_smoothing_spline(voltages, currents)
+
+    u_volt = [u_voltage(i) for i in voltages]
 
     x = np.linspace(voltages[0], voltages[-1], 10000)
     
     smoothed_data = spl(x)
+
+    valley_find_data = smoothed_data.copy()
+    valley_find_data *= -1
+
+    valleys, props = find_peaks(valley_find_data, prominence = 6)
+
+    volt_valleys = [x[i] for i in valleys]
+    curr_valleys = [smoothed_data[i] for i in valleys]
+
+    axs.errorbar(voltages, currents, xerr = u_volt, yerr = u_current, capsize= 3, label = f"Roh {round(rev_bias,2)}")
+    axs.plot(x, smoothed_data, label = f"Spline {round(rev_bias,2)}")
+    ax_3d.plot(x, smoothed_data, zs=rev_bias, zdir='z')
 
     peaks, props = find_peaks(smoothed_data, prominence = 6)
 
     volt_peaks = [x[i] for i in peaks]
     curr_peaks = [smoothed_data[i] for i in peaks]
 
-    u_volt = [u_voltage(i) for i in voltages]
-
     u_volt_peak = [u_voltage(i) for i in volt_peaks]
 
     v0 = 0
     diff = []
 
-    for i in volt_peaks:
-        if i == volt_peaks[0]:
-            v0 = i
-        else:
-            diff.append(i - v0)
-            v0 = i
+    if len(volt_peaks) >= 5:
+        for i in volt_peaks:
+            if i == volt_peaks[0]:
+                v0 = i
+            else:
+                diff.append(i - v0)
+                v0 = i
 
-    energy = [np.mean(diff), np.std(diff)]
+        energy = [np.mean(diff), np.std(diff)]
 
-    print(energy)
+        print(energy)
+        axs.errorbar(volt_peaks, curr_peaks, xerr = u_volt_peak, yerr = u_current, capsize = 3, fmt = "o", label = f"Peaks {round(rev_bias,2)}")
+        axs.errorbar(volt_valleys, curr_valleys, fmt = "o", label = f"Valleys {round(rev_bias,2)}")
 
-    axs.errorbar(voltages, currents, xerr = u_volt, yerr = u_current, capsize= 3, label = f"Roh {round(rev_bias,2)}")
-    axs.plot(x, smoothed_data, label = f"Spline {round(rev_bias,2)}")
-    ax_3d.plot(x, smoothed_data, zs=rev_bias, zdir='z')
-    axs.errorbar(volt_peaks, curr_peaks, xerr = u_volt_peak, yerr = u_volt_peak, capsize = 3, fmt = "o", label = f"Peaks {round(rev_bias,2)}")
+        if len(peaks) == len(valleys):
+            contrast = [(curr_peaks[i] - curr_valleys[i]) / (curr_peaks[i] + curr_valleys[i]) for i in range(len(peaks))]
+            contrast = [np.mean(contrast), np.std(contrast)]
+            return energy, contrast
+        return energy, "Nan"
+    return "Nan", "Nan"
 
-    return energy
+def main():
+    # get all files in the directory
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
-if __name__ == "__main__":
-    reverse_bias_num = {
-        0.5 : 6,
-        1   : 7,
-        1.5 : 8,
-        2   : 9,
-        2.5 : 5,
-        3   : 10,
-        3.5 : 11,
-        4   : 12,
-        4.5 : 13
-    }
-
-    differences = {}
+    onlyfiles.sort()
 
     fig, axs = plt.subplots()
     fig_diff, axs_diff = plt.subplots()
+    fig_contrast, axs_contrast = plt.subplots()
     ax_3d = plt.figure().add_subplot(projection='3d')
 
-    for i in reverse_bias_num.keys():
-        energy = single_evaluation(reverse_bias_num[i], axs, i, ax_3d)
-        axs_diff.errorbar(i, energy[0], yerr = energy[1], capsize = 3, fmt = "o", label = f"{round(i,2)} A") #xerr = 0.5 * (2 * np.sqrt(6)),
+    rev_bias_contrast_li = []
+    contrast_li = []
+    u_contrast_li = []
+    energy_li = []
 
-    rev_biases = [
-        0.0,
-        1.1111111111111112,
-        2.2222222222222223,
-        3.3333333333333335,
-        4.4444444444444445,
-        5.5555555555555555,
-        6.6666666666666667,
-        7.777777777777779
-    ]
+    for i in onlyfiles:
+        # get the rev bias from one of the filenames
+        split = i.split("_")
+        rev_bias = float(split[-2])
+        voltages, currents = csv_read(i)
+        energy, contrast = single_evaluation(voltages, currents, axs, rev_bias, ax_3d)
+        if energy != "Nan":
+            energy_li.append(energy[0])
+            axs_diff.errorbar(rev_bias, energy[0], yerr = energy[1], capsize = 3, fmt = "o", label = f"{round(rev_bias,2)} A") #xerr = 0.5 * (2 * np.sqrt(6)),
+        if contrast != "Nan":
+            rev_bias_contrast_li.append(rev_bias)
+            contrast_li.append(contrast[0])
+            u_contrast_li.append(contrast[1])
+    
+    energy_li.pop(0)
+    energy_li.pop(2)
 
-    for i in rev_biases:
-        energy = single_evaluation(13, axs, i, ax_3d, og_kind = False)
-        axs_diff.errorbar(i, energy[0], yerr = energy[1], capsize = 3, fmt = "o", label = f"{round(i,2)} A") #xerr = 0.5 * (2 * np.sqrt(6)),
+    print(np.mean(energy_li))
+    print(np.std(energy_li))
+
+    axs_contrast.errorbar(rev_bias_contrast_li, contrast_li, yerr = u_contrast_li, fmt = "o", capsize = 3)
 
     # edit style for plots
-    axs.legend(loc = 2, ncol = 3)
+    axs.legend(loc = 2, ncol = 3, fontsize = 6)
     axs.grid()
     axs.set_xlabel(r'Kinetische Energie $E$ [eV]')
     axs.set_ylabel(r'Spannung $U$ [mV]')
@@ -119,7 +130,15 @@ if __name__ == "__main__":
     if test_phase == False: # save figure
         fig.savefig(fname = rf"Alle_Messungen_v1.pdf", format = "pdf")
 
-    # edit style for plots
+    axs_contrast.legend(loc = 4, ncol = 2)
+    axs_contrast.grid()
+    axs_contrast.set_xlabel(r'Gegenspannung $U_G$ [V]')
+    axs_contrast.set_ylabel(r'Kontrast $K$ [mV]')
+    axs_contrast.set_title(rf'Kontrast gegen Gegenspannung')
+    plt.show()
+    if test_phase == False: # save figure
+        fig_contrast.savefig(fname = rf"Messung_contrast_v1.pdf", format = "pdf")
+
     axs_diff.legend(loc = 4, ncol = 2)
     axs_diff.grid()
     axs_diff.set_xlabel(r'Gegenspannung $U_G$ [V]')
@@ -127,6 +146,10 @@ if __name__ == "__main__":
     axs_diff.set_title(rf'Energie gegen Gegenspannung')
     plt.show()
     if test_phase == False: # save figure
-        fig_diff.savefig(fname = rf"Messung_eval_v1.pdf", format = "pdf")
+        fig_diff.savefig(fname = rf"Messung_energy_v1.pdf", format = "pdf")
         
     plt.show()
+    
+
+if __name__ == "__main__":
+    main()
